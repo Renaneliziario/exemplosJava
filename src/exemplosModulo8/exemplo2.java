c6e38ff80222ce16fb2f9852bbcd1d4b963493d6
Renaneliziario <renanqueiroz92@gmail.com>
2025-02-17 21:04:22 -0300

    modulo8 exemplos

---
+++ src/exemplosModulo8/exemplo2.java
@@ -0,0 +1,8 @@
+package exemplosModulo8;
+
+public class Exemplo2 {
+
+    public static void main (String args []) {
+        Exemplos exemplos =  new Exemplos(10);
+    }
+}
---
+++ src/exemplosModulo8/exemplosMod8.java
@@ -0,0 +1,43 @@
+package exemplosModulo8;
+
+public class Exemplos {
+
+    private int codigo;
+
+    private int codigoMaior;
+
+    private float valorDecimal;
+
+    private double valorDecimal;
+
+    private String texto;
+
+    public Exemplos(int val){
+        this.codigo = val;
+    }
+
+    private boolean status;
+
+    private short shor;
+
+    private byte bi;
+
+
+
+    public String retornarTexto(){
+        this.codigo = 0;
+        return "renan";
+    }
+
+    public int retornarInteiro() {
+        int val = 10;
+        String texto = "Texto";
+        this.texto = null
+        return val;
+    }
+
+    public long retornarLong(long num ) {
+        this.texto = "asds";
+        return num;
+    }
+}
